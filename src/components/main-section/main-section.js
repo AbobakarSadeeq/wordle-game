9418ac29d3afa45b50af0e738df1500a8b320c8c
Abobakar Sadeeq <abobakarkhan7@gmail.com>
2023-08-02 08:16:17 +0500

    done

--- src/components/main-section/main-section.js
+++ src/components/main-section/main-section.js
@@ -4,7 +4,7 @@ import TriesBoxesComponent from "../tries-boxes/tries-boxes";
 import MainSectionCss from "./main-section.module.css";
 
 
- 
+
 
 
 const MainSectionComponent = () => {
@@ -21,7 +21,7 @@ const MainSectionComponent = () => {
     setKeyPress((val)=> singleChar);
   }
 
-  // calling removing data from char list on child component 
+  // calling removing data from char list on child component
   const childRef  = useRef();
 
   function removeCharacterHandler(){
@@ -33,7 +33,6 @@ const MainSectionComponent = () => {
   function enterWordHandler(){
     childRef.current.submitWordHandler();
   }
-
 
   // keyboard styling handling
   function keyboardAlphKey(arr){
@@ -54,6 +53,9 @@ const MainSectionComponent = () => {
     renderingTriesWithVals =  <TriesBoxesComponent selectedKeyValue={keyPress} ref={childRef} keyboardStylingArr={keyboardAlphKey}  />
   }
 
+
+
+
   return (
     <>
       <div className={MainSectionCss["main-screen-section"]}>
@@ -69,3 +71,4 @@ const MainSectionComponent = () => {
 };
 
 export default MainSectionComponent;
+
